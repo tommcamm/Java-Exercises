54de82366a0251bf382ddc57e265ccd039c2453d
Tommaso Cammelli <tomm.cammelli@outlook.it>
2019-10-07 16:59:33 +0200

    VarTypes and README file

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tomm's Exercises Repo
+
+Here you can find all of my java exercises for my programming 1 course.
---
+++ VarTypes.java
@@ -0,0 +1,23 @@
+public class VarTypes{
+
+    public static void main(String[] args){
+
+        //Java primitives variables below
+        //-Integers:
+        byte integer1; //From -128 to 127 | 1 Byte used
+        short integer2; //From -32.768 to 32.767 | 2 Byte used
+        int integer3; //From -2.147.483.648 to 2.147.483.647 | 4 Byte used
+        long integer4; //From -9223372036854775808 to 9223372036854775807 | 8 Byte used
+
+        //-floating point variables:
+        float flt1  = 23.554f; //From ±1.4E-45 to ±3.4028235E+38 | 4 Byte used   ||   f must be placed behind the last decimal, or a cast must be made.
+        float flt2  = (float) 3.5324; //Cast example
+        double flt3 = 34.5235; //From ±4.9E-324 to ±1.7976931348623157E+308 | 8 Byte used
+
+        //-Other primitives:
+        boolean bool1; //True or False | 1 bit used (theorical size, depends on jvm used)
+        char char1; //Contains a unicode character | 2 Byte used
+
+
+    }
+}
