cb829817bd0507e30d6d30a2a749cdce7adccda8
tommcamm <tomm.cammelli@outlook.it>
2019-10-09 11:44:13 +0200

    Added modulo and updated .gitignore

---
+++ Modulo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Modulo{
+
+    //Calcolo del modulo di un numero (Lez. 9 Ott.)
+    public static void main(String[] args){
+
+        System.out.print("Benvenuto nel calcolatore del modulo,\n inserisci il primo numero: ");
+        Scanner scn = new Scanner(System.in);
+
+        double firstDoub = scn.nextDouble();
+
+        System.out.print("\nSecondo numero: ");
+        double seondDoub = scn.nextDouble();
+
+        System.out.println("\nIl modulo è: " + firstDoub % secondDoub);
+    }
+}
