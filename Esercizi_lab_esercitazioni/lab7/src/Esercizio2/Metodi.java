57553db5ea2f0b5262563d35c59d7b7bd2d833ba
Tommaso Cammelli <tomm.cammelli@outlook.it>
2019-12-04 21:43:24 +0100

    Moved dir properly

---
+++ Esercizi_lab_esercitazioni/lab7/src/Esercizio2/Metodi.java
@@ -0,0 +1,18 @@
+package Esercizio2;
+
+public class Metodi {
+
+    public static String estraiStringaCasuale(){
+        String[] words = {"telolo", "alberto", "orologio", "esplosione"};
+
+        return words[(int) (Math.random()*4)];
+    }
+
+    public static void stampaLettereIndovinate(String lettere){
+
+    }
+
+    public static boolean scopriLettera(){
+
+    }
+}
---
+++ Esercizi_lab_esercitazioni/lab7/src/Esercizio2/Programma.java
@@ -0,0 +1,15 @@
+package Esercizio2;
+
+import java.util.Scanner;
+
+public class Programma {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        boolean cycle = true;
+
+        do{
+
+
+        }while(cycle);
+    }
+}
