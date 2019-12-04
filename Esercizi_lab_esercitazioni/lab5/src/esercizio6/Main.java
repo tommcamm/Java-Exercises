c8056aec6038bf4822c3d61f64059376a8b0ad58
Tommaso Cammelli <tomm.cammelli@outlook.it>
2019-12-04 21:41:31 +0100

    Renamed lab5 -> lab6

---
+++ Esercizi_lab_esercitazioni/lab5/src/esercizio6/Main.java
@@ -0,0 +1,12 @@
+package esercizio6;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        
+    }
+}
