cace67535ff667d2a14ed9bd6e956b6ca0731566
Tommaso Cammelli <tomm.cammelli@outlook.it>
2019-12-05 16:56:01 +0100

    Terminato esercizio 2 lab 7

--- Esercizi_lab_esercitazioni/lab7/src/Esercizio2/Metodi.java
+++ Esercizi_lab_esercitazioni/lab7/src/Esercizio2/Metodi.java
@@ -4,15 +4,28 @@ public class Metodi {
 
     public static String estraiStringaCasuale(){
         String[] words = {"telolo", "alberto", "orologio", "esplosione"};
-
         return words[(int) (Math.random()*4)];
     }
 
-    public static void stampaLettereIndovinate(String lettere){
-
+    public static void scopriLettera(char[] solution, char[] result, char answer){
+        char lastchar = ' ';
+        for(int i = 0; i<solution.length; i++){
+            if((solution[i] == answer) && (solution[i] != result[i])){
+                if(lastchar != result[i]){
+                    System.out.println("Trovata nuova lettera " + answer + "!"); // Evitiamo ripetizioni quando ha trovato la lettera
+                    lastchar = result[i];
+                }
+                result[i] = solution[i];
+            } else if ((solution[i] == answer) && (solution[i] == result[i])){
+                System.out.println("Lettera " + answer + " già trovata!" );
+                break;
+            }
+        }
     }
 
-    public static boolean scopriLettera(){
-
+    public static void stampaLettereIndovinate(char[] result){
+        for (char c : result) {  // Questo è una versione particolare di ciclo for chiamato foreach... (si potrebbe usare tranquillamente un for classico), oppure il metodo Array.toString [usato dopo]
+            System.out.print(c);
+        }
     }
 }
--- Esercizi_lab_esercitazioni/lab7/src/Esercizio2/Programma.java
+++ Esercizi_lab_esercitazioni/lab7/src/Esercizio2/Programma.java
@@ -1,15 +1,58 @@
 package Esercizio2;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Programma {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         boolean cycle = true;
+        boolean win;
+        char currentLetter;
+        int tentativi;
+        char[] solutionWord;
+        char[] result;
 
         do{
+            //Inizializzazione pre partita
+            win = true;
+            tentativi = 0;
+            solutionWord = Metodi.estraiStringaCasuale().toCharArray();
+            result = new char[solutionWord.length];
+            Arrays.fill(result, '-'); // Sto riempendo l'array con - (con questo metodo evito di usare un for)
+
+            do{
+                System.out.print("\nInserisci lettera: ");
+                currentLetter = scn.nextLine().charAt(0);
+                Metodi.scopriLettera(solutionWord, result, currentLetter);
+                Metodi.stampaLettereIndovinate(result);
+
+                win = true;
+                for (char c : result) {
+                    if (c == '-') {
+                        win = false;
+                        break;
+                    }
+                }
+                if(win) break;
 
 
+                tentativi++;
+            }while(tentativi < 20);
+
+            if(win){
+                System.out.println("\nHai vinto!\n" +
+                        "Numero di tentativi: " + (tentativi + 1));
+            }else{
+                System.out.println("Hai perso...\n" +
+                        "La parola era: " + Arrays.toString(solutionWord)); //Arrays.toString sempre per evitare un for per stampare il risultato
+            }
+
+            System.out.println("Partita terminata, vuoi farne una altra?[y/n] ");
+            if(scn.nextLine().charAt(0) != 'y'){
+                System.out.println("Good bye!");
+                cycle = false;
+            }
         }while(cycle);
     }
 }
