0c1237e2669e9b51904556183f2becac75c065c9
Tommaso Cammelli <tomm.cammelli@outlook.it>
2019-12-08 18:34:54 +0100

    Terminato esercizio 4 Lab 7

--- Esercizi_lab_esercitazioni/lab7/src/Esercizio4/Metodi.java
+++ Esercizi_lab_esercitazioni/lab7/src/Esercizio4/Metodi.java
@@ -1,4 +1,36 @@
 package Esercizio4;
 
 public class Metodi {
+
+    public static int[][] generaMatriceCasuale(int righe, int colonne){
+        int[][] result = new int[righe][colonne];
+
+        for(int i=0; i<righe; i-=-1){
+            for(int ii=0; ii<colonne; ii-=-1){
+                result[i][ii] = (int) (Math.random() * 100);
+            }
+        }
+        return result;
+    }
+
+    public static int[][] trasponiMatrice(int[][] preMatrix){
+        int[][] matrix = new int[preMatrix[0].length][preMatrix.length];
+
+        for(int i=0; i<matrix.length; i++){
+            for(int ii=0; ii<matrix[0].length; ii++){
+                matrix[i][ii] = preMatrix[ii][i];
+            }
+        }
+
+        return matrix;
+    }
+
+    public static void stampaMatrice(int[][] matrix){
+        for(int i=0; i<matrix.length; i++){
+            for(int ii=0; ii<matrix[0].length; ii++){
+                System.out.print(matrix[i][ii] + " ");
+            }
+            System.out.println("");
+        }
+    }
 }
--- Esercizi_lab_esercitazioni/lab7/src/Esercizio4/Programma.java
+++ Esercizi_lab_esercitazioni/lab7/src/Esercizio4/Programma.java
@@ -4,6 +4,20 @@ import java.util.Scanner;
 
 public class Programma {
     public static void main(String[] args) {
+        int righe, colonne;
+        int[][] matrix;
         Scanner scn = new Scanner(System.in);
+
+        System.out.print("Inserisci il numero di righe: ");
+        righe = scn.nextInt();
+
+        System.out.print("Inserisci il numero di colonne: ");
+        colonne = scn.nextInt();
+        matrix = Metodi.generaMatriceCasuale(righe, colonne);
+
+        System.out.println("Matrice non trasposta: ");
+        Metodi.stampaMatrice(matrix); //Osservo la matrice 1
+        System.out.println("\nMatrice trasposta: ");
+        Metodi.stampaMatrice(Metodi.trasponiMatrice(matrix));
     }
 }
